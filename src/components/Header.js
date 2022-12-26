596cbeff3604a498c1904ac079465549fee67913
rofa <zainrofa29@gmail.com>
2022-12-26 15:10:34 +0700

    fix header

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,17 +5,24 @@ import withNavigate from "../helpers/withNavigate";
 // import HeaderLogin from "../components/HeaderLogin";
 // import NavbarSignup from "../components/NavbarSignup";
 
+import defaultImg from "../assets/img/default-photo.png";
 import sakaLogo from "../assets/img/sakacoffee.png";
 import searching from "../assets/img/Searching.png";
 import chat from "../assets/img/chat.png";
 import { getProfile } from "../helpers/fetch";
+import Button from "react-bootstrap/Button";
+import Modal from "react-bootstrap/Modal";
+import { useDispatch, useSelector } from "react-redux";
+import { getProfileActions } from "../redux/actions/profile";
 
 function Header({ navigate }) {
   const [state, setState] = useState("");
   const text = state.text;
   const title = state.title;
-  const [profile, setProfile] = useState({});
+  // const [profile, setProfile] = useState({});
+  const profile = useSelector((state) => state.profile.profile);
   const [search, setSearch] = useState(() => "");
+  const [show, setShow] = useState(false);
   // console.log(element);
 
   const token = JSON.parse(localStorage.getItem("userInfo"))
@@ -47,23 +54,37 @@ function Header({ navigate }) {
     return navigate(`/product?search=${search}`);
   };
 
-  const getDataProfile = async () => {
+  // const getDataProfile = async () => {
+  //   try {
+  //     const result = await getProfile();
+  //     // console.log(result.data.result[0]);
+  //     setProfile(result.data.result[0]);
+  //     console.log(result);
+  //   } catch (error) {
+  //     // console.log(error);
+  //     // console.log(error.response.data.statusCode);
+  //     if (error.response.data.statusCode === 403) {
+  //       navigate("/login");
+  //     }
+  //   }
+  // };
+
+  const handleLogout = async () => {
     try {
-      const result = await getProfile();
-      // console.log(result.data.result[0]);
-      setProfile(result.data.result[0]);
-      console.log(result);
+      localStorage.removeItem("userInfo");
+      navigate("/login");
+      handleClose();
     } catch (error) {
-      // console.log(error);
-      // console.log(error.response.data.statusCode);
-      if (error.response.data.statusCode === 403) {
-        navigate("/login");
-      }
+      console.log(error);
     }
   };
 
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
+  const dispatch = useDispatch();
+
   useEffect(() => {
-    getDataProfile();
+    dispatch(getProfileActions());
   }, []);
 
   return (
@@ -152,31 +173,62 @@ function Header({ navigate }) {
         )}
       </div>
       {token ? (
-        <section className={text}>
-          <form className={styles.searching} onSubmit={getSearch}>
-            <input
-              className={title}
-              type="text"
-              placeholder="search here ..."
-              onChange={setValue}
-            />
-            <div className={styles["search-img"]} onClick={searchBar}>
-              <img src={searching} alt="searching" />
+        role === "Admin" ? (
+          <section className={text}>
+            <form className={styles.searching} onSubmit={getSearch}>
+              <input
+                className={title}
+                type="text"
+                placeholder="search here ..."
+                onChange={setValue}
+              />
+              <div className={styles["search-img"]} onClick={searchBar}>
+                <img src={searching} alt="searching" />
+              </div>
+            </form>
+            <div className={styles.chat}>
+              <div className={styles.notif}>1</div>
+              <img src={chat} alt="" />
             </div>
-          </form>
-          <div className={styles.chat}>
-            <div className={styles.notif}>1</div>
-            <img src={chat} alt="" />
-          </div>
-          <div
-            className={styles.profile}
-            onClick={() => {
-              navigate("/profile");
-            }}
-          >
-            <img src={`http://localhost:8060/${profile.image}`} alt="profile" />
-          </div>
-        </section>
+            <div
+              className={styles.profile}
+              // onClick={() => {
+              //   navigate("/profile");
+              // }}
+            >
+              <button onClick={handleShow}>Logout</button>
+            </div>
+          </section>
+        ) : (
+          <section className={text}>
+            <form className={styles.searching} onSubmit={getSearch}>
+              <input
+                className={title}
+                type="text"
+                placeholder="search here ..."
+                onChange={setValue}
+              />
+              <div className={styles["search-img"]} onClick={searchBar}>
+                <img src={searching} alt="searching" />
+              </div>
+            </form>
+            <div className={styles.chat}>
+              <div className={styles.notif}>1</div>
+              <img src={chat} alt="" />
+            </div>
+            <div
+              className={styles.profile}
+              onClick={() => {
+                navigate("/profile");
+              }}
+            >
+              <img
+                src={`http://localhost:8060/${profile.image}`}
+                alt="profile"
+              />
+            </div>
+          </section>
+        )
       ) : (
         <section className={text}>
           <div className={styles["right-bar"]}>
@@ -208,6 +260,20 @@ function Header({ navigate }) {
         <span></span>
         <span></span>
       </div>
+      <Modal show={show} onHide={handleClose}>
+        <Modal.Header closeButton>
+          <Modal.Title>Saka Coffee Shop</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>Kamu yakin mau keluar?</Modal.Body>
+        <Modal.Footer>
+          <Button className={styles["close-btn"]} onClick={handleClose}>
+            Close
+          </Button>
+          <Button className={styles["yes-btn"]} onClick={handleLogout}>
+            Iyah
+          </Button>
+        </Modal.Footer>
+      </Modal>
     </nav>
   );
 }
